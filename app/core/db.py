90b19d62074892adab3f956b916fb19b5819e506
vq <v.koolko@gmail.com>
2023-02-10 12:04:07 +0300

    init

---
+++ app/core/db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker, declared_attr
+
+from app.core.config import settings
+
+
+class PreBase:
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=PreBase)
+
+engine = create_async_engine(settings.database_url)
+
+syncSessionLocal = sessionmaker(engine, class_=AsyncSession)
---
+++ app/main.py
@@ -0,0 +1,6 @@
+
+from fastapi import FastAPI
+
+from app.core.config import settings
+
+app = FastAPI(title=settings.app_title)
